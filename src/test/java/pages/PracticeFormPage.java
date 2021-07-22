3aadb5c6a603a2f452e1ad393a0e900d2fd5840b
Andrei Kaliasinski <andykolesinski@gmail.com>
2021-07-23 00:06:54 +0300

    Added assertions

--- src/test/java/pages/PracticeFormPage.java
+++ src/test/java/pages/PracticeFormPage.java
@@ -1,11 +1,129 @@
 package pages;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selenide.$x;
 
 public class PracticeFormPage {
 
-    public PracticeFormPage openPracticeFormPage(){
+    public PracticeFormPage openPracticeFormPage() {
         Selenide.open("/automation-practice-form");
+        $x("//*[@class='main-header']").shouldHave(Condition.text("Practice Form"));
+        return this;
+    }
+
+    public PracticeFormPage enterFirstName(String firstName) {
+        $x("//*[@id='firstName']").val("Larry");
+        return this;
+    }
+
+    public PracticeFormPage enterLastName(String lastName) {
+        $x("//*[@id='lastName']").val(lastName);
+        return this;
+    }
+
+    public PracticeFormPage enterEmail(String email) {
+        $x("//*[@id='userEmail']").val(email);
+        return this;
+    }
+
+    public PracticeFormPage selectGender(String gender) {
+        $x("//*[@id='genterWrapper']//*[text()='" + gender + "']").click();
+        return this;
+    }
+
+    public PracticeFormPage enterMobileNumber(String mobileNumber) {
+        $x("//*[@id='userNumber']").val(mobileNumber);
+        return this;
+    }
+
+    public PracticeFormPage selectDateOfBirth(String year, String month, String day) {
+        $x("//*[@id='dateOfBirthInput']").click();
+        $x("//*[@class='react-datepicker__year-select']").selectOption(year);
+        $x("//*[@class='react-datepicker__month-select']").selectOption(month);
+        $x(String.format("//div[contains(@class, 'react-datepicker__day--0%s') and " +
+                "not(contains(@class, 'react-datepicker__day--outside-month'))]", day)).click();
+        return this;
+    }
+
+    public PracticeFormPage enterSubject(String subject) {
+        $x("//*[@id='subjectsInput']").val(subject).pressEnter();
+        return this;
+    }
+
+    public PracticeFormPage uploadPicture(String path) {
+        $x("//*[@id='uploadPicture']").uploadFile(new File(path));
+        return this;
+    }
+
+    public PracticeFormPage enterCurrentAddress(String address) {
+        $x("//*[@id='currentAddress']").val(address);
+        return this;
+    }
+
+    public PracticeFormPage selectState(String state) {
+        $x("//*[@id='state']").scrollTo().click();
+        $x(String.format("//*[@id='stateCity-wrapper']//*[text()='%s']", state)).click();
+        return this;
+    }
+
+    public PracticeFormPage selectCity(String city) {
+        $x("//*[@id='city']").scrollTo().click();
+        $x(String.format("//*[@id='stateCity-wrapper']//*[text()='%s']", city)).click();
+        return this;
+    }
+
+    public PracticeFormPage clickSubmitButton() {
+        $x("//*[@id='submit']").click();
+        return this;
+    }
+
+    public PracticeFormPage checkStudentName(String studentName) {
+        $x("//td[text()='Student Name']/following-sibling::td").shouldHave(Condition.text(studentName));
+        return this;
+    }
+
+    public PracticeFormPage checkStudentEmail(String studentEmail) {
+        $x("//td[text()='Student Email']/following-sibling::td").shouldHave(Condition.text(studentEmail));
+        return this;
+    }
+
+    public PracticeFormPage checkStudentGender(String studentGender) {
+        $x("//td[text()='Gender']/following-sibling::td").shouldHave(Condition.text(studentGender));
+        return this;
+    }
+
+    public PracticeFormPage checkMobileNumber(String mobileNumber) {
+        $x("//td[text()='Mobile']/following-sibling::td").shouldHave(Condition.text(mobileNumber));
+        return this;
+    }
+
+    public PracticeFormPage checkDateOfBirth(String dateOfBirth) {
+        $x("//td[text()='Date of Birth']/following-sibling::td").shouldHave(Condition.text(dateOfBirth));
+        return this;
+    }
+
+    public PracticeFormPage checkSubjects(String subjects) {
+        $x("//td[text()='Subjects']/following-sibling::td").shouldHave(Condition.text(subjects));
+        return this;
+    }
+
+    public PracticeFormPage checkPicture(String pictureName) {
+        $x("//td[text()='Picture']/following-sibling::td").shouldHave(Condition.text(pictureName));
+        return this;
+    }
+
+    public PracticeFormPage checkAddress(String address) {
+        $x("//td[text()='Address']/following-sibling::td").shouldHave(Condition.text(address));
+        return this;
+    }
+
+    public PracticeFormPage checkStateAndCity(String StateAndCity) {
+        $x("//td[text()='State and City']/following-sibling::td").shouldHave(Condition.text(StateAndCity));
         return this;
     }
 }
--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -3,9 +3,47 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 public class PracticeFormTests extends BaseTest {
+    String firstName = "Larry";
+    String lastName = "Brown";
+    String email = "larrybrown@gmail.com";
+    String gender = "Male";
+    String mobileNumber = "1234567890";
+    String yearOfBirth = "2001";
+    String mothOfBirth = "June";
+    String dayOfBirth = "12";
+    String subject = "Math";
+    String filePath = "src/test/resources/test.jpg";
+    String pictureName = "test.jpg";
+    String address = "Test address";
+    String state = "Haryana";
+    String city = "Karnal";
 
     @Test
-    public void userCanFillAndSubmitTheForm(){
-        practiceFormPage.openPracticeFormPage();
+    public void userCanFillAndSubmitTheForm() {
+        practiceFormPage
+                .openPracticeFormPage()
+                .enterFirstName(firstName)
+                .enterLastName(lastName)
+                .enterEmail(email)
+                .selectGender(gender)
+                .enterMobileNumber(mobileNumber)
+                .selectDateOfBirth(yearOfBirth, mothOfBirth, dayOfBirth)
+                .enterSubject(subject)
+                .uploadPicture(filePath)
+                .enterCurrentAddress(address)
+                .selectState(state)
+                .selectCity(city)
+                .clickSubmitButton();
+
+        practiceFormPage
+                .checkStudentName(firstName + " " + lastName)
+                .checkStudentEmail(email)
+                .checkStudentGender(gender)
+                .checkMobileNumber(mobileNumber)
+                .checkDateOfBirth(dayOfBirth + " " + mothOfBirth + "," + yearOfBirth)
+                .checkSubjects(subject)
+                .checkPicture(pictureName)
+                .checkAddress(address)
+                .checkStateAndCity(state + " " + city);
     }
 }
