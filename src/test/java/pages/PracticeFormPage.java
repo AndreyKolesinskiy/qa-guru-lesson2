f372a0b6fd1d1ce2f1828f96b28c8e5d31f2d47b
Andrei Kaliasinski <andykolesinski@gmail.com>
2021-07-25 10:38:29 +0300

    Imported condition.text statically

--- src/test/java/pages/PracticeFormPage.java
+++ src/test/java/pages/PracticeFormPage.java
@@ -1,15 +1,16 @@
 package pages;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class PracticeFormPage {
 
     public PracticeFormPage openPracticeFormPage() {
         Selenide.open("/automation-practice-form");
-        $x("//*[@class='main-header']").shouldHave(Condition.text("Practice Form"));
+        $x("//*[@class='main-header']").shouldHave(text("Practice Form"));
         return this;
     }
 
@@ -79,47 +80,47 @@ public class PracticeFormPage {
     }
 
     public PracticeFormPage checkStudentName(String studentName) {
-        $x("//td[text()='Student Name']/following-sibling::td").shouldHave(Condition.text(studentName));
+        $x("//td[text()='Student Name']/following-sibling::td").shouldHave(text(studentName));
         return this;
     }
 
     public PracticeFormPage checkStudentEmail(String studentEmail) {
-        $x("//td[text()='Student Email']/following-sibling::td").shouldHave(Condition.text(studentEmail));
+        $x("//td[text()='Student Email']/following-sibling::td").shouldHave(text(studentEmail));
         return this;
     }
 
     public PracticeFormPage checkStudentGender(String studentGender) {
-        $x("//td[text()='Gender']/following-sibling::td").shouldHave(Condition.text(studentGender));
+        $x("//td[text()='Gender']/following-sibling::td").shouldHave(text(studentGender));
         return this;
     }
 
     public PracticeFormPage checkMobileNumber(String mobileNumber) {
-        $x("//td[text()='Mobile']/following-sibling::td").shouldHave(Condition.text(mobileNumber));
+        $x("//td[text()='Mobile']/following-sibling::td").shouldHave(text(mobileNumber));
         return this;
     }
 
     public PracticeFormPage checkDateOfBirth(String dateOfBirth) {
-        $x("//td[text()='Date of Birth']/following-sibling::td").shouldHave(Condition.text(dateOfBirth));
+        $x("//td[text()='Date of Birth']/following-sibling::td").shouldHave(text(dateOfBirth));
         return this;
     }
 
     public PracticeFormPage checkSubjects(String subjects) {
-        $x("//td[text()='Subjects']/following-sibling::td").shouldHave(Condition.text(subjects));
+        $x("//td[text()='Subjects']/following-sibling::td").shouldHave(text(subjects));
         return this;
     }
 
     public PracticeFormPage checkPicture(String pictureName) {
-        $x("//td[text()='Picture']/following-sibling::td").shouldHave(Condition.text(pictureName));
+        $x("//td[text()='Picture']/following-sibling::td").shouldHave(text(pictureName));
         return this;
     }
 
     public PracticeFormPage checkAddress(String address) {
-        $x("//td[text()='Address']/following-sibling::td").shouldHave(Condition.text(address));
+        $x("//td[text()='Address']/following-sibling::td").shouldHave(text(address));
         return this;
     }
 
     public PracticeFormPage checkStateAndCity(String StateAndCity) {
-        $x("//td[text()='State and City']/following-sibling::td").shouldHave(Condition.text(StateAndCity));
+        $x("//td[text()='State and City']/following-sibling::td").shouldHave(text(StateAndCity));
         return this;
     }
 }
